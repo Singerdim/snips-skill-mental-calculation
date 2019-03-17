61e4683251eb12d7c595e571133dab55686383b1
Singerdim <9867460+Singerdim@users.noreply.github.com>
2019-03-17 15:24:22 +0100

    Update action-mental-calculation.py

--- action-mental-calculation.py
+++ action-mental-calculation.py
@@ -6,10 +6,10 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-INTENT_START = "bezzam:start_mental_calculations"
-INTENT_ANSWER = "bezzam:give_mental_calculation"
-INTENT_STOP = "bezzam:stop_lesson"
-INTENT_DOES_NOT_KNOW = "bezzam:does_not_know_calculation"
+INTENT_START = "singerdim:CommenceCalculMental"
+INTENT_ANSWER = "singerdim:DonneReponse"
+INTENT_STOP = "singerdim:FinDuTest"
+INTENT_DOES_NOT_KNOW = "singerdim:NeSaisPas"
 
 INTENT_FILTER_GET_ANSWER = [
     INTENT_ANSWER,
@@ -35,18 +35,18 @@ def create_question(oper=None):
     answer = None
     if oper == "add":
         answer = x + y
-        question = "What is {} plus {}?".format(x, y)
+        question = "Combien font {} plus {}?".format(x, y)
     elif oper == "sub":
         answer = x
         x = x + y
-        question = "What is {} minus {}?".format(x, y)
+        question = "Combien font {} moins {}?".format(x, y)
     elif oper == "mul":
         answer = x * y
-        question = "What is {} times {}?".format(x, y)
+        question = "Combien font {} fois {}?".format(x, y)
     elif oper == "div":
         answer = x
         x = x * y
-        question = "What is {} divided by {}?".format(x, y)
+        question = "Combien font {} divisés par {}?".format(x, y)
 
     return question, answer
 
@@ -55,17 +55,17 @@ def continue_lesson(response, session_id):
     SessionsStates[session_id]["step"] += 1
 
     if SessionsStates[session_id]["step"] == SessionsStates[session_id]["n_questions"]:
-        response += "You had {} out of {} correct. ".format(SessionsStates[session_id]["good"],
+        response += "Vous avez {} sur {} correctes. ".format(SessionsStates[session_id]["good"],
                                                                              SessionsStates[session_id]["n_questions"])
         percent_correct = float(SessionsStates[session_id]["good"]) / SessionsStates[session_id]["n_questions"]
         if percent_correct == 1.:
-            response += "You are so smart!"
+            response += "Vous etes au top!"
         elif percent_correct >= 0.75:
-            response += "Well done! With a bit more practice you'll be a master."
+            response += "Bien joué! Avec un peu plus d'entrainement vous serez au top."
         elif percent_correct >= 0.5:
-            response += "Not bad. With more practice, you'll get better."
+            response += "Pas mal. Vous devriez progresser en vous entrainant."
         else:
-            response += "You should really practice more."
+            response += "Il faut travailler les mathematiques."
         del SessionsStates[session_id]
         cont = False
     else:
@@ -81,13 +81,13 @@ def user_request_quiz(hermes, intent_message):
     session_id = intent_message.session_id
 
     # parse input message, NOTE extra space to append question
-    n_questions = int(intent_message.slots.n_questions.first().value)
+    n_questions = int(intent_message.slots.NbreQuestions.first().value)
     if n_questions > 1:
-        response = "Starting a lesson with {} questions. ".format(n_questions)
+        response = "On commence un test avec {} questions. ".format(n_questions)
     elif n_questions == 1:
-        response = "Starting a lesson with one question. "
+        response = "On commence un test avec une question. "
     else:
-        response = "I can only give a positive number of questions."
+        response = "Je ne peux que vous faire un tesst avec un nombre positif de questions."
         hermes.publish_end_session(session_id, response)
 
     # create first question
@@ -110,14 +110,14 @@ def user_gives_answer(hermes, intent_message):
     session_id = intent_message.session_id
 
     # parse input message
-    answer = intent_message.slots.answer.first().value
+    answer = intent_message.slots.Reponse.first().value
 
     # check user answer, NOTE the extra space at the end since we will add more to the response!
     if answer == SessionsStates[session_id]["ans"]:
         response = "Correct! "
         SessionsStates[session_id]["good"] += 1
     else:
-        response = "Incorrect. The answer is {}. ".format(SessionsStates[session_id]["ans"])
+        response = "Faux. La reponse est {}. ".format(SessionsStates[session_id]["ans"])
         SessionsStates[session_id]["bad"] += 1
 
     # create new question or terminate if reached desired number of questions
@@ -131,7 +131,7 @@ def user_gives_answer(hermes, intent_message):
 def user_does_not_know(hermes, intent_message):
     session_id = intent_message.session_id
 
-    response = "That's quite alright! The answer is {}. ".format(SessionsStates[session_id]["ans"])
+    response = "C'est pas grave! La reponse est {}. ".format(SessionsStates[session_id]["ans"])
 
     # create new question or terminate if reached desired number of questions
     response, cont = continue_lesson(response, session_id)
@@ -146,7 +146,7 @@ def user_quits(hermes, intent_message):
 
     # clean up
     del SessionsStates[session_id]
-    response = "Alright. Let's play again soon!"
+    response = "Tres bien. ON s'arrete là mais recommencons ca tres vite!"
 
     hermes.publish_end_session(session_id, response)
 
